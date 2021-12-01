fb30bf20d1bb56a688e2550dc431967564dc377d
boliqueso <harold1799@gmail.com>
2021-12-01 18:22:44 -0500

    actualizar nombre de micro app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/front-cordova'),
+      dir: require('path').join(__dirname, './coverage/front-coroda'),
       subdir: '.',
       reporters: [
         { type: 'html' },
