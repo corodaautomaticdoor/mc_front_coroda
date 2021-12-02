2e5f4354512e0787dfcbeb57032d43e1ed42cb9e
boliqueso <harold1799@gmail.com>
2021-12-01 21:32:53 -0500

    endpoints

--- src/environments/environment.ssr.ts
+++ src/environments/environment.ssr.ts
@@ -3,5 +3,6 @@ export const environment = {
     url: 'http://localhost:4000',
     API_PRODUCTO_URL: 'https://cad-mc-product.herokuapp.com/',
     API_LOGIN_URL: 'https://cad-mc-login.herokuapp.com/',
+    API_PERSON_URL: 'https://cad-mc-profile.herokuapp.com/',
     API_OPERATION_URL: 'https://cad-mc-operation.herokuapp.com/'
 };
